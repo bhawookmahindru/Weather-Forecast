afb656352408b02bf9a74af630f9439ea03490a7
Bhawook Mahindru <bhawookmahindru@gmail.com>
2021-07-19 12:06:08 +0530

    my project

---
+++ app/src/main/java/com/example/bhawook54545434/weatherforecast/MainActivity.kt
@@ -0,0 +1,76 @@
+package com.example.bhawook54545434.weatherforecast
+
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.JsonRequest
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import kotlinx.android.synthetic.main.activity_main.*
+import org.json.JSONObject
+import kotlin.math.ceil
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val lat = intent.getStringExtra("lat")
+        val long = intent.getStringExtra("long")
+
+        window.statusBarColor = Color.parseColor("#D7DBDD")
+
+        getJsonData(lat, long)
+
+    }
+
+    private fun getJsonData(lat: String?, long: String?) {
+        // Instantiate the RequestQueue.
+        val API_key = "90b79dd518da4b7e4a42f2b6f5edb30b"   // put you API Key here
+        val queue = Volley.newRequestQueue(this)
+        val url =
+            "https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${API_key}"
+
+        // Request a string response from the provided URL.
+        // val can be change to string or any other variable
+        val JsonRequest = JsonObjectRequest(
+            Request.Method.GET, url, null,
+            Response.Listener { response ->
+                setValues(response)
+            },
+            Response.ErrorListener {
+                Toast.makeText(this, "Error", Toast.LENGTH_SHORT).show()
+            })
+
+        // Add the request to the RequestQueue.
+        queue.add(JsonRequest)
+    }
+
+    private fun setValues(response: JSONObject) {
+        city_name.text = response.getString("name")
+        var lat = response.getJSONObject("coord").getString("lat")
+        var long = response.getJSONObject("coord").getString("lon")
+        coordinates.text = "${lat},${long}"
+        weather.text = response.getJSONArray("weather").getJSONObject(0).getString("main")
+        var tempr = response.getJSONObject("main").getString("temp")
+        tempr = ((((tempr).toFloat() - 273.15)).toInt()).toString()
+        temp.text = "${tempr}°C"
+        var mintemp = response.getJSONObject("main").getString("temp_min")
+        mintemp = ((((mintemp).toFloat() - 273.15)).toInt()).toString()
+        min_temp.text = mintemp + "°C"
+        var maxtemp = response.getJSONObject("main").getString("temp_min")
+        maxtemp = ((ceil((maxtemp).toFloat() - 273.15)).toInt()).toString()
+        max_temp.text = maxtemp + "°C"
+        pressure.text = response.getJSONObject("main").getString("pressure")
+        humidity.text = response.getJSONObject("main").getString("humidity") + "%"
+        wind.text = response.getJSONObject("wind").getString("speed")
+        degree.text = "Degree : " + response.getJSONObject("wind").getString("deg") + "°"
+        /*  gust.text = "Gust : " + response.getJSONObject("wind").getString("gust") + "°"*/
+
+    }
+
+}
---
+++ app/src/main/java/com/example/bhawook54545434/weatherforecast/SplashScreen.kt
@@ -0,0 +1,126 @@
+package com.example.bhawook54545434.weatherforecast
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Color
+import android.location.Location
+import android.location.LocationManager
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.util.Log
+import android.widget.TextView
+import android.widget.Toast
+import androidx.annotation.RequiresPermission
+import androidx.core.app.ActivityCompat
+import com.airbnb.lottie.LottieAnimationView
+import com.google.android.gms.location.*
+import kotlinx.android.synthetic.main.activity_splash_screen.*
+
+
+class SplashScreen : AppCompatActivity() {
+    lateinit var mfusedlocation: FusedLocationProviderClient
+    private var myRequestCode = 1010
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        window.statusBarColor = Color.parseColor("#73CDFA")
+
+        mfusedlocation = LocationServices.getFusedLocationProviderClient(this)
+
+        getLastLocation()
+
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun getLastLocation() {
+        if (CheckPermission()) {
+            if (LocationEnable()) {
+                mfusedlocation.lastLocation.addOnCompleteListener { task ->
+                    var location: Location? = task.result
+                    if (location == null) {
+                        NewLocation()
+                    } else {
+                        Handler(Looper.getMainLooper()).postDelayed({
+                            var intent = Intent(this, MainActivity::class.java)
+                            intent.putExtra("lat", location.latitude.toString())
+                            intent.putExtra("long", location.longitude.toString())
+                            startActivity(intent)
+                            finish()
+                        }, 2000)
+                    }
+                }
+            } else {
+                Toast.makeText(this, "Please turn on your GPS location", Toast.LENGTH_SHORT).show()
+            }
+        } else {
+            RequestPermission()
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun NewLocation() {
+        var locationRequest = LocationRequest()
+        locationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        locationRequest.interval = 0
+        locationRequest.fastestInterval = 0
+        locationRequest.numUpdates = 1
+        mfusedlocation = LocationServices.getFusedLocationProviderClient(this)
+        mfusedlocation.requestLocationUpdates(locationRequest, locationCallBack, Looper.myLooper())
+    }
+
+    private var locationCallBack = object : LocationCallback() {
+        override fun onLocationResult(p0: LocationResult) {
+            var lastlocation: Location = p0.lastLocation
+        }
+    }
+
+    private fun LocationEnable(): Boolean {
+        var locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(
+            LocationManager.NETWORK_PROVIDER
+        )
+    }
+
+    private fun RequestPermission() {
+        ActivityCompat.requestPermissions(
+            this, arrayOf(
+                android.Manifest.permission.ACCESS_COARSE_LOCATION,
+                android.Manifest.permission.ACCESS_FINE_LOCATION
+            ), myRequestCode
+        )
+    }
+
+    private fun CheckPermission(): Boolean {
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED ||
+            ActivityCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.ACCESS_COARSE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            return true
+        }
+        return false
+
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == myRequestCode) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                getLastLocation()
+            }
+        }
+    }
+
+}
